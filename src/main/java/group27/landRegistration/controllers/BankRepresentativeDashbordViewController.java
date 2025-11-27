cbf45f44721de2f0d4866cfa3911be0b50bf03fc
mdnomanpaloan-bd <mdnomanpaloan@gmail.com>
2025-11-27 13:35:17 +0600

    Noman Added Bank And Survey DashBoard

---
+++ src/main/java/group27/landRegistration/controllers/BankRepresentativeDashbordViewController.java
@@ -0,0 +1,41 @@
+package group27.landRegistration.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+
+public class BankRepresentativeDashbordViewController {
+    @javafx.fxml.FXML
+    private DatePicker DateofPaymentSlipDatePicker;
+
+    @javafx.fxml.FXML
+    public void TransactionLogsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SendPaymentNoticeOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BacktoHomePageOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ValiddatePaymentProofOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void RefundCorrectionOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ApproveMortgageonAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void VerifyPaymentonAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ReconcileTransactionsOnAction(ActionEvent actionEvent) {
+    }
+}
