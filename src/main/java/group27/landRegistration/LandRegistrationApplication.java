fffde7475915733efd2232fceb1a63f9a0022f7e
NewYearTobirama <kn.nahyan01@gmail.com>
2025-11-27 14:09:43 +0600

    Nahyan deleted HelloController, helloView, and updated launcher and LandRegistrationApplication

--- src/main/java/group27/landRegistration/LandRegistrationApplication.java
+++ src/main/java/group27/landRegistration/LandRegistrationApplication.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class LandRegistrationApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LoginView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(LandRegistrationApplication.class.getResource("LoginView.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Land Registration of Bangladesh");
         stage.setScene(scene);
