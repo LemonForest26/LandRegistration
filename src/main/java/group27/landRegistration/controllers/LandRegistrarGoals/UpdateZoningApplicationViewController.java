40c92a753177273dd68df6d8c00f22b3aa2ff552
LemonForest26 <lemonforest.26@gmail.com>
2025-11-27 23:35:46 +0600

    Labib desinged zoning part!

--- src/main/java/group27/landRegistration/controllers/LandRegistrarGoals/UpdateZoningApplicationViewController.java
+++ src/main/java/group27/landRegistration/controllers/LandRegistrarGoals/UpdateZoningApplicationViewController.java
@@ -1,4 +1,14 @@
 package group27.landRegistration.controllers.LandRegistrarGoals;
 
 public class UpdateZoningApplicationViewController {
+    @javafx.fxml.FXML
+    private TextField PlotIDTF;
+    @javafx.fxml.FXML
+    private TextArea FileUploadTA;
+    @javafx.fxml.FXML
+    private ComboBox NewZoningCB;
+
+    @javafx.fxml.FXML
+    public void SaveZoning(ActionEvent actionEvent) {
+    }
 }
