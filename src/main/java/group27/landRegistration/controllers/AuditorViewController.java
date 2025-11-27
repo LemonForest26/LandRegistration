eb942035b46a12972192604b43c79f57fdcb8720
LemonForest26 <lemonforest.26@gmail.com>
2025-11-27 14:06:00 +0600

    Labib updated dashboard controllers of Land Registrar and Auditor.

--- src/main/java/group27/landRegistration/controllers/AuditorViewController.java
+++ src/main/java/group27/landRegistration/controllers/AuditorViewController.java
@@ -1,4 +1,39 @@
 package group27.landRegistration.controllers;
 
 public class AuditorViewController {
+    @javafx.fxml.FXML
+    public void ValuationCheckOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void UserPermissionOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void MonthlyReportLockOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ApprovalSamplingOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ActivityLogOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void LogOutOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void IntegritySummaryOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackupManagerOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void GenerateAuditReportOA(ActionEvent actionEvent) {
+    }
 }
--- src/main/java/group27/landRegistration/controllers/LandRegistrarDashBoardViewController.java
+++ src/main/java/group27/landRegistration/controllers/LandRegistrarDashBoardViewController.java
@@ -1,4 +1,41 @@
 package group27.landRegistration.controllers;
 
+import javafx.event.ActionEvent;
+
 public class LandRegistrarDashBoardViewController {
+    @javafx.fxml.FXML
+    public void OfficerPerformanceOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void GenerateCertificateOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ZoningUpdateOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void DailySummaryReportOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ApproveRejectOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ReviewPendingApplicationsOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void LogOutOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void DisputesOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void TransferOwnershipOA(ActionEvent actionEvent) {
+    }
 }
