599c0df22b467206279eb934d94cb34e5f788a3a
LemonForest26 <lemonforest.26@gmail.com>
2025-11-28 22:37:56 +0600

    Labib created GenerateAuditReport fxml and controller.

---
+++ src/main/java/group27/landRegistration/controllers/AuditorGoals/GenerateAuditReportViewController.java
@@ -0,0 +1,19 @@
+package group27.landRegistration.controllers.AuditorGoals;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+
+public class GenerateAuditReportViewController {
+    @javafx.fxml.FXML
+    private DatePicker StartDateDP;
+    @javafx.fxml.FXML
+    private DatePicker EndDateDP;
+
+    @javafx.fxml.FXML
+    public void GenerateREportOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackOA(ActionEvent actionEvent) {
+    }
+}
