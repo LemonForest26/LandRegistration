266dfebddbf59251eb87556c8fac8ce476f289dd
NewYearTobirama <kn.nahyan01@gmail.com>
2025-11-28 22:37:24 +0600

    Nahyan helped with Auditor goals.

---
+++ src/main/java/group27/landRegistration/controllers/AuditorGoals/ValuationCheckViewController.java
@@ -0,0 +1,29 @@
+package group27.landRegistration.controllers.AuditorGoals;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ValuationCheckViewController {
+    @javafx.fxml.FXML
+    private TextField PlotIDTF;
+    @javafx.fxml.FXML
+    private TableColumn UserTC;
+    @javafx.fxml.FXML
+    private TableColumn AreaTC;
+    @javafx.fxml.FXML
+    private TableView PlotDetailsTV;
+    @javafx.fxml.FXML
+    private TableColumn CertifiedTC;
+    @javafx.fxml.FXML
+    private TableColumn TimestampTC;
+
+    @javafx.fxml.FXML
+    public void MarkForReviewOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackOA(ActionEvent actionEvent) {
+    }
+}
