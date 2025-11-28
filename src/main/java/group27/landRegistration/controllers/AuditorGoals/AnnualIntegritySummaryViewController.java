0ba2da71fec5577462fde5b08080caee45a9f268
LemonForest26 <lemonforest.26@gmail.com>
2025-11-28 23:59:17 +0600

    Labib created two more goals of Auditor fxml and controller.

---
+++ src/main/java/group27/landRegistration/controllers/AuditorGoals/AnnualIntegritySummaryViewController.java
@@ -0,0 +1,20 @@
+package group27.landRegistration.controllers.AuditorGoals;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+
+public class AnnualIntegritySummaryViewController {
+    @javafx.fxml.FXML
+    private TextArea PreviewTA;
+    @javafx.fxml.FXML
+    private ComboBox YearDP;
+
+    @javafx.fxml.FXML
+    public void BackOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void CompileOA(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/group27/landRegistration/controllers/AuditorGoals/ApprovalSamplingViewController.java
@@ -0,0 +1,44 @@
+package group27.landRegistration.controllers.AuditorGoals;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ApprovalSamplingViewController {
+    @javafx.fxml.FXML
+    private ComboBox RegistrarNameCB;
+    @javafx.fxml.FXML
+    private TableColumn StatusTC;
+    @javafx.fxml.FXML
+    private TableView ApprovalSamplingTV;
+    @javafx.fxml.FXML
+    private TableColumn PlotIDTC;
+    @javafx.fxml.FXML
+    private TableColumn ApprovedDateTC;
+    @javafx.fxml.FXML
+    private DatePicker StartDateDP;
+    @javafx.fxml.FXML
+    private DatePicker EndDateDP;
+    @javafx.fxml.FXML
+    private TableColumn ApplicationNameTC;
+    @javafx.fxml.FXML
+    private TableColumn ApplicationIDTC;
+
+    @javafx.fxml.FXML
+    public void FilterRegistrarOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void DateFilterOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void FlagSampleOA(ActionEvent actionEvent) {
+    }
+}
