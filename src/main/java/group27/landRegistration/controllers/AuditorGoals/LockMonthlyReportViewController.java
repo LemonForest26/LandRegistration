86c1e5e8531f876573b5a8cf653a433aafb9eaca
LemonForest26 <lemonforest.26@gmail.com>
2025-11-29 11:26:07 +0600

    Labib fixed Lock Monthly report goal of Auditor...

--- src/main/java/group27/landRegistration/controllers/AuditorGoals/LockMonthlyReportViewController.java
+++ src/main/java/group27/landRegistration/controllers/AuditorGoals/LockMonthlyReportViewController.java
@@ -1,6 +1,7 @@
 package group27.landRegistration.controllers.AuditorGoals;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
