928f4ea2fc5b32375d419dbee798ab699fb96447
mdnomanpaloan-bd <mdnomanpaloan@gmail.com>
2025-12-02 16:52:31 +0600

    Noman Make Payment Verification PieChart Commit And Push

--- src/main/java/group27/landRegistration/users/BankRepresentative.java
+++ src/main/java/group27/landRegistration/users/BankRepresentative.java
@@ -7,17 +7,5 @@ public class BankRepresentative extends User {
     public BankRepresentative(int userID, String name, String password, String email, String gender, long NID, long phoneNumber, LocalDate doB) {
         super(userID, name, password, email, gender, NID, phoneNumber, doB);
     }
-    // Approve a mortgage request
-//    public boolean approveMortgage(String applicationId) {
-//        // TODO: Replace with real database or service logic
-//        System.out.println("Mortgage approved for application ID: " + applicationId);
-//        return true;
-//    }
-//
-//    // Reconcile a transaction
-//    //public boolean reconcileTransaction(String transactionId) {
-//        // TODO: Replace with real reconciliation logic
-//        System.out.println("Transaction reconciled for ID: " + transactionId);
-//        return true;
 
     }
