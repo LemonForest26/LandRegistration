018d695917d062c5d79e9666de1ce2321ab909e8
mdnomanpaloan-bd <mdnomanpaloan@gmail.com>
2025-12-02 16:45:27 +0600

    Noman Make Payment Verification PieChart Commit And Push

---
+++ src/main/java/group27/landRegistration/controllers/BankRepresentativeGoals/PaymentVerificationPieChartViewController.java
@@ -0,0 +1,17 @@
+package group27.landRegistration.controllers.BankRepresentativeGoals;
+
+import javafx.event.ActionEvent;
+import javafx.scene.chart.PieChart;
+
+public class PaymentVerificationPieChartViewController {
+    @javafx.fxml.FXML
+    private PieChart PaymentVerificationPieChart;
+
+    @javafx.fxml.FXML
+    public void BackOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void PaymentVerificationPieChartOnAction(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/group27/landRegistration/users/BankRepresentative.java
+++ src/main/java/group27/landRegistration/users/BankRepresentative.java
@@ -7,4 +7,17 @@ public class BankRepresentative extends User {
     public BankRepresentative(String name, String password, String email, long NID, long phoneNumber, LocalDate doB) {
         super(name, password, email, NID, phoneNumber, doB);
     }
-}
+    // Approve a mortgage request
+//    public boolean approveMortgage(String applicationId) {
+//        // TODO: Replace with real database or service logic
+//        System.out.println("Mortgage approved for application ID: " + applicationId);
+//        return true;
+//    }
+//
+//    // Reconcile a transaction
+//    //public boolean reconcileTransaction(String transactionId) {
+//        // TODO: Replace with real reconciliation logic
+//        System.out.println("Transaction reconciled for ID: " + transactionId);
+//        return true;
+
+    }
