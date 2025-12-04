1c1fe3b1d459c0731552ff357267e1778dcfc71c
LemonForest26 <lemonforest.26@gmail.com>
2025-12-04 11:09:12 +0600

    Labib updated correct way data transfer from dashboard to a goal page.

--- src/main/java/group27/landRegistration/controllers/AllDashboards/LandRegistrarDashBoardViewController.java
+++ src/main/java/group27/landRegistration/controllers/AllDashboards/LandRegistrarDashBoardViewController.java
@@ -24,9 +24,6 @@ public class LandRegistrarDashBoardViewController {
         return loggedInUser;
     }
 
-
-
-
     @javafx.fxml.FXML
     public void OfficerPerformanceOA(ActionEvent actionEvent) {
         try {
